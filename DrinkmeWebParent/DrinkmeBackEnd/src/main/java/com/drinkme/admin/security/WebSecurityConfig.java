7377e89737d58978ddab54523717d3f17d282435
pierpaolopazzi <pierpaolopazzi23@gmail.com>
2024-01-12 14:59:46 +0100

    Updated WebSecurityConfig.java;Added login cookies

--- DrinkmeWebParent/DrinkmeBackEnd/src/main/java/com/drinkme/admin/security/WebSecurityConfig.java
+++ DrinkmeWebParent/DrinkmeBackEnd/src/main/java/com/drinkme/admin/security/WebSecurityConfig.java
@@ -45,7 +45,11 @@ public class WebSecurityConfig {
 						.loginPage("/login")
 						.usernameParameter("email")
 						.permitAll())
-				.logout(logout -> logout.permitAll());
+				.logout(logout -> logout.permitAll())
+				
+				.rememberMe(rem -> rem
+						.key("AbcDefgHijKlmnOpqrs_1234567890")
+						.tokenValiditySeconds(7 * 24 * 60 * 60));
 		
 		return http.build();
 	}
